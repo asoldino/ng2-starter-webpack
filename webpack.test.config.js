73cd68e537190f27486d92b336f7b46460023b78
Andrea Soldino <andrea.soldino@gmail.com>
2016-01-29 15:36:07 +0100

    Fixed webpack test configuration

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -58,10 +58,21 @@ module.exports = {
                 },
                 exclude: [/\.e2e\.ts$/]
             },
-            {test: /\.json$/, loader: 'json-loader'},
+
+            
+            {test: /\.ts$/, loader: 'ts-loader'},
             {test: /\.html$/, loader: 'html-loader'},
-            {test: /\.css$/, loader: 'raw-loader'},
-            {test: /\.jade/, loader: 'jade'}
+            {test: /\.css$/, loader: 'css-loader'},
+            {test: /\.jade$/, loader: 'jade'},
+
+            {test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery'},
+
+            // Needed for the css-loader when [bootstrap-webpack](https://github.com/bline/bootstrap-webpack)
+            // loads bootstrap's css.
+            {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
         ],
         postLoaders: [
             // instrument only testing sources with Istanbul
