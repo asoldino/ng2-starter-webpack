fd1267a90f963c770eb45aaf9d011f4816825f36
Andrea Soldino <andrea.soldino@gmail.com>
2016-02-01 11:03:48 +0100

    Added bootstrap support and related loaders

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -31,7 +31,8 @@ module.exports = {
                 test: /\.js$/,
                 loader: "source-map-loader",
                 exclude: [
-                    path.resolve(__dirname, '../rxjs')
+                    path.resolve(__dirname, '../rxjs'),
+                    path.resolve(__dirname, '../bootstrap-webpack')
                 ]
             }
         ],
@@ -61,7 +62,16 @@ module.exports = {
             {test: /\.json$/, loader: 'json-loader'},
             {test: /\.html$/, loader: 'html-loader'},
             {test: /\.css$/, loader: 'raw-loader'},
-            {test: /\.jade/, loader: 'jade'}
+            {test: /\.jade/, loader: 'jade'},
+
+            {test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery'},
+
+            // Needed for the css-loader when [bootstrap-webpack](https://github.com/bline/bootstrap-webpack)
+            // loads bootstrap's css.
+            {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
         ],
         postLoaders: [
             // instrument only testing sources with Istanbul
